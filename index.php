c28e2a99e17b487309655d43164caeffddf393c1
jgiordano01 <joey.giordano@gmail.com>
2015-07-30 17:45:27 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
-<html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
- <?php echo '<p>Hello Joe World Version 1</p>'; ?> 
- </body>
-</html>
+<html> 
+<head> 
+<title>Personal INFO</title> 
+</head> 
+<body> 
+<form method="post" action="<?php echo $PHP_SELF;?>">
+First Name:<input type="text" size="12" maxlength="12" name="Fname">:<br />
+Last Name:<input type="text" size="12" maxlength="36" name="Lname">:<br />
